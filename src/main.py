6b48fcd2749f96a95fd64a6c9eaf22c1d2534995
potexxi <maximilian.schreiter@outlook.at>
2025-05-08 16:55:25 +0200

    2,5h Max

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import pygame
+import submodule.globals as g
 
 
 def main():
@@ -8,6 +9,16 @@ def main():
 
     # Initialization
     pygame.init()
+
+    # Screen and Caption
+    screen = pygame.display.set_mode((g.WIDTH, g.HEIGHT))
+    pygame.display.set_caption("Flip Happens")
+
+    running = True
+    while running:
+        pass
+        # Todo: weitermachen
+
 
 
 if __name__ == "__main__":
---
+++ src/submodule/globals.py
@@ -0,0 +1,7 @@
+import pygame
+
+pygame.init()
+
+# Height and Width of the window
+HEIGHT = int(pygame.display.Info().current_h - 60)
+WIDTH = int(pygame.display.Info(). current_w)
