79995c79a6e68f27dd76d1bb3fb746341537490c
potexxi2 <potexxi.tt@gmail.com>
2025-05-09 19:00:58 +0200

    Zwischenspeichern vom Hauptmenu

--- src/submodule/menu.py
+++ src/submodule/menu.py
@@ -1,3 +1,5 @@
+from xml.sax.saxutils import prepare_input_source
+
 import pygame
 import src.submodule.globals as g
 
@@ -26,9 +28,17 @@ def draw_button(screen: pygame.Surface, text: str, button: tuple[float, float, f
     screen.blit(text, (button[0], button[1]))
 
 
+def draw_flip_happens(screen: pygame.Surface, size: int, position: tuple[float, float]) -> None:
+    font = pygame.font.SysFont("Impact", size, False, False)
+    text = font.render("FLIP HAPPENS!", False, 'black')
+    text = pygame.transform.rotate(text, 90)
+    screen.blit(text, position)
+
+
 def menu(screen: pygame.Surface) -> None:
     screen.blit(background, (0,0))
-    draw_button(screen, "Hallo", (g.WIDTH/2-100,g.HEIGHT/3,g.WIDTH/10,g.HEIGHT/10))
+    draw_flip_happens(screen, 100, (100, 500))
+    draw_button(screen, "Hallo", (g.WIDTH/2-100,g.HEIGHT/3,g.WIDTH/10,g.HEIGHT/10)) # Todo: Buttons machen
 
 
     return None
