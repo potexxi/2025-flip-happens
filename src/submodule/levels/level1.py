1fabf8762eacb4f23f48b82d08035cf5ceaeeb6f
potexxi2 <potexxi.tt@gmail.com>
2025-05-11 16:11:19 +0200

    level1.py

--- src/submodule/levels/level1.py
+++ src/submodule/levels/level1.py
@@ -5,6 +5,7 @@ from src.submodule.menu import draw_button, check_button_collide
 background: pygame.Surface = None
 brick: pygame.Surface = None
 ui_pause: pygame.Surface = None
+halfpipe: pygame.Surface = None
 mode: str = "play"
 pause_button = [g.WIDTH - g.WIDTH / 50 -5, 5, g.WIDTH / 50, g.WIDTH / 50]
 
@@ -14,7 +15,7 @@ def init_level1() -> None:
     """
     Init all the pictures the level 1 needs
     """
-    global background, brick, ui_pause
+    global background, brick, ui_pause, halfpipe
     # Background:
     background = pygame.image.load("assets/level1/background.png").convert_alpha()
     background = pygame.transform.scale(background, (g.WIDTH, g.HEIGHT))
@@ -25,6 +26,9 @@ def init_level1() -> None:
     # Pause Button:
     ui_pause = pygame.image.load("assets/ui-controls/menu.png").convert_alpha()
     ui_pause = pygame.transform.scale(ui_pause, (pause_button[2],pause_button[3]))
+    # Halfpipe:
+    halfpipe = pygame.image.load("assets/level1/halfpipe.png")
+    halfpipe = pygame.transform.scale(halfpipe, (g.ASSETS_SIZE, g.ASSETS_SIZE))
 
 
 def check_menu_button_pressed(screen: pygame.Surface) -> str:
@@ -42,6 +46,21 @@ def check_menu_button_pressed(screen: pygame.Surface) -> str:
     return "play"
 
 
+def place_bricks(screen: pygame.Surface) -> None:
+    # Floor:
+    for t in range(g.WIDTH // g.ASSETS_SIZE + 1):
+        screen.blit(brick, (0 + g.ASSETS_SIZE * t - 30, g.HEIGHT - (g.ASSETS_SIZE - g.ASSETS_SIZE // 2)))
+        if t == 15:
+            for y in range(4):
+                screen.blit(brick, ((g.ASSETS_SIZE * t - 30) + y*g.ASSETS_SIZE,
+                                    g.HEIGHT - g.ASSETS_SIZE - (g.ASSETS_SIZE - g.ASSETS_SIZE // 2)))
+
+
+def place_elements(screen: pygame.Surface) -> None:
+    # Ramps:
+    screen.blit(halfpipe, ((g.ASSETS_SIZE * 15 - 30) - g.ASSETS_SIZE, g.HEIGHT - g.ASSETS_SIZE - 36))
+
+
 def level1(screen: pygame.Surface) -> str:
     """
     level1 of the game
@@ -51,10 +70,9 @@ def level1(screen: pygame.Surface) -> str:
     global mode
     # Background:
     screen.blit(background, (0,0))
-    # Bricks:
-    # Floor:
-    for t in range(g.WIDTH//g.ASSETS_SIZE + 1):
-        screen.blit(brick, (0 + g.ASSETS_SIZE * t - 30,g.HEIGHT - (g.ASSETS_SIZE - g.ASSETS_SIZE//2)))
+    # Elements:
+    place_bricks(screen)
+    place_elements(screen)
     # Pause - button
     mode = check_menu_button_pressed(screen)
     return mode
